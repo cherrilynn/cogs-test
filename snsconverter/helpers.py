f9f8fe694af801152acdf8a96bf9d6541c95fc7f
cherrilynn <cherriesandsnow@gmail.com>
2024-06-23 16:50:17 -0400

    Update helpers.py

--- snsconverter/helpers.py
+++ snsconverter/helpers.py
@@ -149,7 +149,6 @@ def urls_to_string(links: list[str], socialMedia: SocialMedia):
     """
     return "\n".join(
         [
-            "Removing text",
             f"*fixes your {socialMedia.value} embeds:*",
             *links,
         ]
