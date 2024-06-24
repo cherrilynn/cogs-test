4c0e125d501a21e6eb55075070714e257b9cb2b4
cherrilynn <cherriesandsnow@gmail.com>
2024-06-24 16:14:18 -0400

    Update eventsCore.py

--- snsconverter/eventsCore.py
+++ snsconverter/eventsCore.py
@@ -103,12 +103,12 @@ class EventsCore(Core):
             return
 
         # constructs the message and replies with a mention
-        
-        await message.reply(urls_to_string(fx_twtter_urls, SocialMedia.TWITTER), mention_author=False)
+
+        if any(substring in message.content.lower() for substring in [www.x.com]):
+            msg = await message.reply(urls_to_string(fx_twtter_urls, SocialMedia.TWITTER), mention_author=False)
+            await message.edit(suppress=True)
         
         # Remove embeds from user message if reply is successful
-        
-        await message.edit(suppress=True)
     
 
     async def _on_edit_twit_replacer(self, message_before: Message, message_after: Message):
@@ -131,11 +131,9 @@ class EventsCore(Core):
             return
 
         # constructs the message and replies with a mention
-        await message_after.reply(urls_to_string(fx_twtter_urls, SocialMedia.TWITTER), mention_author=False)
-        
-        # Remove embeds from user message if reply is successful
-        
-        await message_after.edit(suppress=True)
+        if any(substring in message.content.lower() for substring in [www.x.com]):
+            msg = await message.reply(urls_to_string(fx_twtter_urls, SocialMedia.TWITTER), mention_author=False)
+            await message_after.edit(suppress=True)
 
     async def _on_message_tik_replacer(self, message: Message):
         if not valid(message):
